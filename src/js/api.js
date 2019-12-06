e58e5c3b4f368358e444787c0712d1aba4f72605
Alexander Sirbu <sirbualexandru92@gmail.com>
2019-12-06 20:09:38 +0200

    -

--- src/js/api.js
+++ src/js/api.js
@@ -2,10 +2,10 @@
 const API_KEY = '7429042c97e6a05117b067c14694f822'
 
 export function getApi (city) {
-  return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
     .then((response) => response.json())
 }
 export function getApiDaily (city) {
-  return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
     .then((response) => response.json())
 }
